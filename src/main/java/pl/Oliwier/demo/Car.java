a77f07f44c1d731357ad9c5030f13b536e548447
s22018 <s22018@pjwstk.edu.pl>
2021-10-16 10:20:40 +0200

    Car_commit

---
+++ src/main/java/pl/Oliwier/demo/Car.java
@@ -0,0 +1,17 @@
+package pl.Oliwier.demo;
+
+public class Car {
+    private String Model;
+    private float Engine;
+    private String Color;
+    private int Scales;
+
+    Car(String Model,float Engine,String Color,int Scales){
+        this.Model=Model;
+        this.Engine=Engine;
+        this.Color=Color;
+        this.Scales=Scales;
+
+    }
+    
+}
